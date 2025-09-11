76903549f6dd3365648fa788d1152d27f43cb2bc
FERNANDA JUAREZ <lizgar239@gmail.com>
2025-09-11 11:26:13 -0600

    funciones ejemplo

--- JSParaPrincipiantes/JSParaPrincipiantes.js
+++ JSParaPrincipiantes/JSParaPrincipiantes.js
@@ -1,41 +1,10 @@
-var nameuser="john";
-let lastname="carter";
-// OPERADORES
-let numberOne = 100;
-let numberTwo = 500;
-let count = 0;
-//obtener info de un arreglo
-let names = ['ryan', 'joe', 'john', 'mario'];
-
-nameuser='pepe';
-const PI = 3.1415;
-console.log(PI)
-letnumberOne=60;
-let numberTwo=100;
-let result = numberOne > numberTwo;
-
-let userName = 'gordon';let result = numberOne + numberTwo;
-
-console.log(result); console.log(result);if(result == true) {
-    console.log('Login correcto');
-} else {
-    console.log('contraseña incorrecta');
-}if (score > 30) {
-    console.log('You need to practice more');
-} else if (score > 15) {
-    console.log('Estas mejorando');
+function add(n1, n2) {
+    console.log(n1 + n2);
 }
-else{
-    console.log('You need to folow this tutorial');
- }switch(typeCard) {
-    case 'Debid Card':
-        console.log('This is a debid Card');
-        break;
-        case 'Credit Card':
-            console.log('This is a cedit card');
-            break;
-while(count > 0) {
-while(count < 50) {
-    console.log(count);
-    count++;
-}    console.log(names.length);
+
+add(3,2);
+add(100,300);
+
+add(3, 2);
+add(100, 300);
+add(31213212, 12132312);
