28754d941cc2138200fe9d7fc9b92194b83942fa
FERNANDA JUAREZ <lizgar239@gmail.com>
2025-09-11 01:02:40 -0600

    ejemplo condicionales

--- JSParaPrincipiantes/JSParaPrincipiantes.js
+++ JSParaPrincipiantes/JSParaPrincipiantes.js
@@ -17,4 +17,11 @@ console.log(result); console.log(result);if(result == true) {
     console.log('Login correcto');
 } else {
     console.log('contraseña incorrecta');
+}if (score > 30) {
+    console.log('You need to practice more');
+} else if (score > 15) {
+    console.log('Estas mejorando');
 }
+else{
+    console.log('You need to folow this tutorial');
+ }
