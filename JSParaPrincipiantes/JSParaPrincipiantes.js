a534d369a0b73d4ee569333bb31b6bd16d885cf9
FERNANDA JUAREZ <lizgar239@gmail.com>
2025-09-11 00:39:22 -0600

    ejemplo operadores y se refleja en consola

--- JSParaPrincipiantes/JSParaPrincipiantes.js
+++ JSParaPrincipiantes/JSParaPrincipiantes.js
@@ -1,13 +1,16 @@
 var nameuser="john";
 let lastname="carter";
 // OPERADORES
+let numberOne = 100;
+let numberTwo = 500;
 
 nameuser='pepe';
 const PI = 3.1415;
 console.log(PI)
 letnumberOne=60;
 let numberTwo=100;
+let result = numberOne > numberTwo;
 
 let userName = 'gordon';let result = numberOne + numberTwo;
 
-console.log(result);
+console.log(result); console.log(result);
