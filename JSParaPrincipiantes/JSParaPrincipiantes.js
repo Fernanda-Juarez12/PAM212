a03002bae287005270148ecbc141fa62b2ee5965
FERNANDA JUAREZ <lizgar239@gmail.com>
2025-09-11 00:29:52 -0600

    ejemplo operadores

--- JSParaPrincipiantes/JSParaPrincipiantes.js
+++ JSParaPrincipiantes/JSParaPrincipiantes.js
@@ -1,8 +1,13 @@
 var nameuser="john";
 let lastname="carter";
+// OPERADORES
 
 nameuser='pepe';
 const PI = 3.1415;
 console.log(PI)
+letnumberOne=60;
+let numberTwo=100;
 
-let userName = 'gordon';
+let userName = 'gordon';let result = numberOne + numberTwo;
+
+console.log(result);
