2195fd3df53056cdf78d42d62cb9cd8d2de37109
FERNANDA JUAREZ <lizgar239@gmail.com>
2025-09-11 00:17:51 -0600

    Tipos de Datos explicación

--- JSParaPrincipiantes/JSParaPrincipiantes.js
+++ JSParaPrincipiantes/JSParaPrincipiantes.js
@@ -1,1 +1,25 @@
-document.write("<h1> Hello World </h1>")
+Hello World" //string
+'Hello World' //string
+// maneras de escribirlos
+
+// boolean
+true
+false
+
+// array
+['joe','ryan','martha']
+[1, 2, 3]
+
+//object, conformado por pares clave
+'ryan'
+70.4
+14
+true
+
+// crear objeto a traves de llaves
+{
+    "username": 'ryan'
+    "score": 70.4,
+    "hours": 14, 
+    "proffesional": true,
+}
