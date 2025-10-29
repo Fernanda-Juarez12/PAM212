2846a38715af29111095c247695dc2d9ef76ded9
FERNANDA JUAREZ <lizgar239@gmail.com>
2025-10-28 23:09:00 -0600

    PRACTICA 13 ACTIVITYINDICATOR Y FLATLIST

--- intro/screens/ActivityIndicatorScreen.js
+++ intro/screens/ActivityIndicatorScreen.js
@@ -1,12 +1,75 @@
-import { Text, StyleSheet, View } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, Button, ActivityIndicator, StyleSheet } from 'react-native-web';
 
 
 export default function ActivityIndicatorScreen() {
-    return (
-      <View>
-        <Text>ActivityIndicatorScreen Proximamente...</Text>
-      </View>
-    )
+
+
+const [cargando, setCargando] = useState(false);
+const iniciarCarga = () => {setCargando(true);setTimeout(() => setCargando(false), 3000);};
+const detenerCarga = () => { setCargando(false);};
+
+return (
+    <View style={styles.container}>
+        <Text style={styles.texto}>Práctica: Activity Indicator</Text>
+
+
+        <View style={styles.boton}>
+        <Button color = '#8cd7a5ff'
+            title={cargando ? 'Cargando...' : 'Iniciar carga'}
+            onPress={iniciarCarga}
+        />
+        </View>
+
+
+        <View style={styles.boton}>
+        <Button color = '#ee3e3eff'
+            title="Detener carga" onPress={detenerCarga} />
+        </View>
+
+
+        <View style={styles.carga}>
+        <ActivityIndicator
+            size="large"
+            color="#da38c4ff"
+            animating={cargando}          
+            hidesWhenStopped={true}      
+        />
+
+        <Text style={styles.textoCarga}>
+            {cargando ? 'Cargando datos...' : 'Presiona el botón verde :)'}
+        </Text>
+        </View>
+    </View>
+    );
 }
 
-const styles = StyleSheet.create({})
+
+const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    backgroundColor: '#f5d8d8ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+},
+texto: {
+    color: "#1d0b0bff",
+    fontSize: 30,
+    fontFamily: 'Times New Roman',
+    fontWeight: 'bold',
+    marginBottom: 20,
+},
+boton: {
+    width: 220,
+    marginBottom: 16,
+},
+carga: {
+    alignItems: 'center',
+    marginTop: 20,
+},
+textoCarga: {
+    marginTop: 12,
+    fontSize: 16,
+    color: '#000000',
+},
+});
--- intro/screens/FlatListScreen.js
+++ intro/screens/FlatListScreen.js
@@ -1,12 +1,101 @@
-import { Text, StyleSheet, View } from 'react-native'
+import React from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  SectionList,
+  StyleSheet,
+} from 'react-native';
 
+//  Datos para la FlatList
+const alumnos = [
+  { id: '1', nombre: 'Feer' },
+  { id: '2', nombre: 'Soong Yoong Ki' },
+  { id: '3', nombre: 'Shawn Mendes' },
+  { id: '4', nombre: 'La arrolladora' },
+  { id: '5', nombre: 'Banda Carnaval' },
+];
+
+// Datos para la SectionList
+const categorias = [
+  {
+    titulo: 'Primavera',
+    data: ['Marzo', 'Abril', 'Mayo'],
+  },
+  {
+    titulo: 'Verano',
+    data: ['Junio', 'Julio', 'Agosto'],
+  },
+  {
+    titulo: 'Otoño',
+    data: ['Septiembre', 'Octubre', 'Noviembre'],
+  },
+  {
+    titulo: 'Invierno',
+    data: ['Diciembre', 'Enero', 'Febrero'],
+  },
+];
 
 export default function FlatListScreen() {
-    return (
-      <View>
-        <Text>FlatListScreen Proximamente...</Text>
-      </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Ejemplo de FlatList</Text>
+
+      <FlatList
+        data={alumnos}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.itemBox}>
+            <Text style={styles.itemText}>* {item.nombre}</Text>
+          </View>
+        )}
+      />
+
+      <Text style={styles.title}>Ejemplo de SectionList</Text>
+
+      <SectionList
+        sections={categorias}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => (
+          <Text style={styles.itemText}>• {item}</Text>
+        )}
+        renderSectionHeader={({ section: { titulo } }) => (
+          <Text style={styles.sectionHeader}>{titulo}</Text>
+        )}
+      />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0B0B0B',
+    padding: 20,
+    paddingTop: 60,
+  },
+  title: {
+    fontSize: 20,
+    color: '#f9aff4ff',
+    fontWeight: 'bold',
+    marginBottom: 10,
+    marginTop: 20,
+  },
+  itemBox: {
+    backgroundColor: '#a268faff',
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 8,
+  },
+  itemText: {
+    color: '#fff1f1ff',
+    fontSize: 16,
+  },
+  sectionHeader: {
+    color: '#98cef2ff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 15,
+    marginBottom: 5,
+  },
+});
