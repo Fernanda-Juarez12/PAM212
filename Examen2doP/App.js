e97d9237e7e18c10326d54034f1ec37ed4370e8e
FERNANDA JUAREZ <lizgar239@gmail.com>
2025-11-07 18:51:17 -0600

    COMMIT FINAL:(

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,72 +1,21 @@
-import { Text, StyleSheet, View } from 'react-native'
-import React, { useEffect, useRef, useState } from "react";
-import {
-  View,
-  Text,
-  Animated,
-  StyleSheet,
-  Dimensions,
-  ScrollView
-} from "react-native";
-import * as SplashScreen from "expo-splash-screen";
 
-SplashScreen.preventAutoHideAsync();
+import React from 'react';
+import { ScrollView, StyleSheet, Text, View, SwitchButton, Button, Alert} from 'react-native-web';
 
-const { height } = Dimensions.get("window");
 
-export default function SplashScreenPro() {
-  const [showMain, setShowMain] = useState(false); // controla la pantalla principal
+const DATA = [
+ {id:1},
+ {id:2},
+ {id:3},
+]; 
 
-  const fadeLogo = useRef(new Animated.Value(0)).current;
-  const scaleLogo = useRef(new Animated.Value(0.5)).current;
-  const rotateLogo = useRef(new Animated.Value(0)).current;
-  const slideText = useRef(new Animated.Value(height / 2)).current;
-  const fadeOut = useRef(new Animated.Value(1)).current;
-
-  useEffect(() => {
-    Animated.parallel([
-      Animated.timing(fadeLogo, {
-        toValue: 1,
-        duration: 1200,
-        useNativeDriver: false,
-      }),
-      Animated.spring(scaleLogo, {
-        toValue: 1,
-        friction: 5,
-        useNativeDriver: false,
-      }),
-      Animated.timing(rotateLogo, {
-        toValue: 1,
-        duration: 1200,
-        useNativeDriver: false,
-      }),
-    ]).start();
-
-    Animated.timing(slideText, {
-      toValue: 0,
-      duration: 1000,
-      useNativeDriver: false,
-      delay: 800,
-    }).start();
-
-    const timer = setTimeout(async () => {
-      Animated.timing(fadeOut, {
-        toValue: 0,
-        duration: 800,
-        useNativeDriver: false,
-      }).start(async () => {
-        await SplashScreen.hideAsync(); 
-        setShowMain(true); // 
-      });
-    }, 3000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  const rotateInterpolate = rotateLogo.interpolate({
-    inputRange: [0, 1],
-    outputRange: ["0deg", "10deg"],
-  });
+const SimpleHeader = () => {
+  return (
+    <View style ={styles.header}>
+      <Text style={styles.title}>SECCIONES</Text>
+    </View>
+  );
+};
 
 
 const SimpleScrollView = () => {
@@ -74,33 +23,52 @@ const SimpleScrollView = () => {
     <View>
       <SimpleHeader/>
       <ScrollView 
-      showVerticalScrollIndicator={false}
-      >
+      showVerticalScrollIndicator={false}>
+
         {DATA.map(val => {
           return (
             <View style={styles.card} key = {val.id}>
-              <Text style={styles.subtitle}> DEPORTES</Text>
+              <Text style={styles.subtitle}>DEPORTES</Text>
+            </View>
+          );
+        })}
+
+        {DATA.map(val => {
+          return (
+            <View style={styles.card} key = {val.id}>
+              <Text style={styles.subtitle}>ENTRETENIMIENTO</Text>
+            </View>
+          );
+        })}
+
+        
+        {DATA.map(val => {
+          return (
+            <View style={styles.card} key = {val.id}>
+              <Text style={styles.subtitle}>NOTICIAS</Text>
             </View>
           );
         })}
     </ScrollView>
   </View>
-  );
-};
+
+  )};
+
+
+
 
 export default SimpleScrollView;
-
 
 const styles = StyleSheet.create({
   header: {
     height: 120,
-    backgroundColor: '#407887ff',
+    backgroundColor: '#3b197eff',
     justifyContent: 'center',
     alignItems: 'center',
     paddingTop: 25,
   },
   title: {
-    color: '#fff',
+    color: '#fff8f8ff',
     fontWeigt: 'bold',
     fontSize: 30,
   },
@@ -116,24 +84,4 @@ const styles = StyleSheet.create({
     color: '#181D31',
     fontWeight: 'bold',
   },
-  background: {
-    flex: 1, // ocupa toda la pantalla
-    width: "100%", // ancho completo
-    height: "100%", // alto completo
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  content: {
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    padding: 20,
-    borderRadius: 10,
-  },
-  text: {
-    color: "white",
-    fontSize: 24,
-    marginBottom: 10,
-    textAlign: "center",
-  },
 });
-};
-
