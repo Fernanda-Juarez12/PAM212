d2cbaece46a1d72f4388f30a1f42cbf7b46ee6c8
FERNANDA JUAREZ <lizgar239@gmail.com>
2025-11-07 18:08:40 -0600

    AVANCE EXAMEN 2DOP

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,20 +1,139 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native'
+import React, { useEffect, useRef, useState } from "react";
+import {
+  View,
+  Text,
+  Animated,
+  StyleSheet,
+  Dimensions,
+  ScrollView
+} from "react-native";
+import * as SplashScreen from "expo-splash-screen";
 
-export default function App() {
+SplashScreen.preventAutoHideAsync();
+
+const { height } = Dimensions.get("window");
+
+export default function SplashScreenPro() {
+  const [showMain, setShowMain] = useState(false); // controla la pantalla principal
+
+  const fadeLogo = useRef(new Animated.Value(0)).current;
+  const scaleLogo = useRef(new Animated.Value(0.5)).current;
+  const rotateLogo = useRef(new Animated.Value(0)).current;
+  const slideText = useRef(new Animated.Value(height / 2)).current;
+  const fadeOut = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(fadeLogo, {
+        toValue: 1,
+        duration: 1200,
+        useNativeDriver: false,
+      }),
+      Animated.spring(scaleLogo, {
+        toValue: 1,
+        friction: 5,
+        useNativeDriver: false,
+      }),
+      Animated.timing(rotateLogo, {
+        toValue: 1,
+        duration: 1200,
+        useNativeDriver: false,
+      }),
+    ]).start();
+
+    Animated.timing(slideText, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: false,
+      delay: 800,
+    }).start();
+
+    const timer = setTimeout(async () => {
+      Animated.timing(fadeOut, {
+        toValue: 0,
+        duration: 800,
+        useNativeDriver: false,
+      }).start(async () => {
+        await SplashScreen.hideAsync(); 
+        setShowMain(true); // 
+      });
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const rotateInterpolate = rotateLogo.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "10deg"],
+  });
+
+
+const SimpleScrollView = () => {
   return (
-    <View style={styles.container}>
-      <Text>HOLAAAA</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View>
+      <SimpleHeader/>
+      <ScrollView 
+      showVerticalScrollIndicator={false}
+      >
+        {DATA.map(val => {
+          return (
+            <View style={styles.card} key = {val.id}>
+              <Text style={styles.subtitle}> DEPORTES</Text>
+            </View>
+          );
+        })}
+    </ScrollView>
+  </View>
   );
-}
+};
+
+export default SimpleScrollView;
+
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  header: {
+    height: 120,
+    backgroundColor: '#407887ff',
     justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 25,
+  },
+  title: {
+    color: '#fff',
+    fontWeigt: 'bold',
+    fontSize: 30,
+  },
+  card: {
+    height: 100,
+    backgroundColor: '#e9b6f5ff',
+    marginTop: 10, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  subtitle: {
+    color: '#181D31',
+    fontWeight: 'bold',
+  },
+  background: {
+    flex: 1, // ocupa toda la pantalla
+    width: "100%", // ancho completo
+    height: "100%", // alto completo
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    padding: 20,
+    borderRadius: 10,
+  },
+  text: {
+    color: "white",
+    fontSize: 24,
+    marginBottom: 10,
+    textAlign: "center",
   },
 });
+};
+
